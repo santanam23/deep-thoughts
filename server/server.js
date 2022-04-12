a6283466559f662cfbfa6b9c8f38fcb8f56c7a21
santanam23 <mirmir11.ms@gmail.com>
2022-04-12 15:42:21 -0500

    auth creation

--- server/server.js
+++ server/server.js
@@ -1,36 +1,23 @@
 const express = require('express');
-
-// import AplloServer
 const { ApolloServer } = require('apollo-server-express');
 
-// import our typeDefs and resolvers 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// create Apollo server
 const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
-    resolvers,
-    context: () => {authMiddleware}
+    resolvers
   });
-  // Start the apollo server
   await server.start();
-
-  // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
-
-  // log where we acn go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
 };
-// Initialize the Apollo Server
-startServer();
 
+startServer()
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
