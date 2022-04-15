bfb153ef10fe7e9d1164b854b1ac27823b240cd1
santanam23 <mirmir11.ms@gmail.com>
2022-04-15 13:45:38 -0500

    created client

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,18 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("thoughts")
+          .populate("friends");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
     users: async () => {
       return User.find()
         .select("-__v -password")
@@ -48,6 +60,35 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({...args, username: context.user});
+
+        await User.findByIdAndUpdate(
+          {_id: context.user._id},
+          { $push: { thoughts: thought._id} },
+          { new:true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError('You need to be logged in!')
+    },
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          { $push: { reactions: { reactionBody, username: context.user.username } } },
+          { new: true, runValidators: true }
+        );
+    
+        return updatedThought;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    }
+    
   },
 };
 
