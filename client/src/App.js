06695887404c3cb8a78d0210c71e7f5e0eeeb934
santanam23 <mirmir11.ms@gmail.com>
2022-04-21 16:59:17 -0500

    fixed error

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3001/graphql',
-  });
-  
-  const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-  });
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </ApolloProvider>
   );
 }
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,19 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
-export const QUERY_THOUGHTS = gql `
-query thoughts($username: String) {
+export const QUERY_THOUGHTS = gql`
+  query thoughts($username: String) {
     thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
         _id
-        thoughtText
         createdAt
         username
-        reactionCount
-        reactions {
-            _id
-            createdAt
-            username
-            reactionBody
-        }
+        reactionBody
+      }
     }
-}`
+  }
+`;
